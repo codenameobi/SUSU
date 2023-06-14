3e8407e01610d7264ff93e09ceeb1a9933a45167
obiora-nwude <122012709+obiora-nwude@users.noreply.github.com>
2023-06-14 22:50:11 +0100

    initial commit

---
+++ Susu.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Susu.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ Susu.Maui/obj/Debug/net7.0-android/android/src/crc64e9e196c7498cbde9/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64e9e196c7498cbde9;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Susu.Maui.MainActivity, Susu.Maui", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Susu.Maui.MainActivity, Susu.Maui", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Susu.Maui.MainActivity, Susu.Maui", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
